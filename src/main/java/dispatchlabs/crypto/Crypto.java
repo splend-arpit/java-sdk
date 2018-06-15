77d75b4797469365279dc3427247bfb66b38632c
Christopher McGregor <chris@mcgregor.io>
2018-06-15 11:55:27 -0700

    Clean up

--- src/main/java/dispatchlabs/crypto/Crypto.java
+++ src/main/java/dispatchlabs/crypto/Crypto.java
@@ -1,8 +1,12 @@
 package dispatchlabs.crypto;
 
 import dispatchlabs.states.Transaction;
+import dispatchlabs.utils.Utils;
 import org.bitcoin.NativeSecp256k1;
 import org.bouncycastle.jcajce.provider.digest.Keccak;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.ByteArrayOutputStream;
 
 /**
  *
@@ -10,7 +14,6 @@ import org.bouncycastle.jcajce.provider.digest.Keccak;
 public class Crypto {
 
     /**
-     *
      * @param privateKey
      * @param hash
      * @return
@@ -28,5 +31,42 @@ public class Crypto {
         final Keccak.Digest256 keccak = new Keccak.Digest256();
         keccak.update(bytes);
         return keccak.digest();
+    }
+
+    /**
+     * @param transaction
+     * @return
+     * @throws Exception
+     */
+    private static String createHash(Transaction transaction) throws Exception {
+
+        byte[] typeBytes = {transaction.getType()};
+        byte[] fromBytes = DatatypeConverter.parseHexBinary(transaction.getFrom());
+        byte[] toBytes = DatatypeConverter.parseHexBinary(transaction.getTo());
+        byte[] valueBytes = Utils.longToBytes(transaction.getValue());
+        byte[] timeBytes = Utils.longToBytes(transaction.getTime());
+
+        // Hash bytes.
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byteArrayOutputStream.write(typeBytes);
+        byteArrayOutputStream.write(fromBytes);
+        byteArrayOutputStream.write(toBytes);
+        byteArrayOutputStream.write(valueBytes);
+        if (transaction.getCode() != null) {
+            byte[] codeBytes = DatatypeConverter.parseHexBinary(transaction.getCode());
+            byteArrayOutputStream.write(codeBytes);
+        }
+        if (transaction.getAbi() != null) {
+            byteArrayOutputStream.write(transaction.getAbi().getBytes("UTF-8"));
+        }
+        if (transaction.getMethod() != null) {
+            byteArrayOutputStream.write(transaction.getMethod().getBytes("UTF-8"));
+        }
+        // TODO: Add Params ro hash.
+        byteArrayOutputStream.write(timeBytes);
+
+        byte[] hashBytes = Crypto.hash(byteArrayOutputStream.toByteArray());
+
+        return Utils.toHexString(hashBytes);
     }
 }
